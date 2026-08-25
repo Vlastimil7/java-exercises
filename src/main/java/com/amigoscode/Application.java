a2a6c50039660e80a97bd6d1c35fd91f9aa371a3
Vlastimil Kalášek <25kali25@gmail.com>
2026-08-25 21:23:50 +0200

    Developers Loops finished.

--- src/main/java/com/amigoscode/Application.java
+++ src/main/java/com/amigoscode/Application.java
@@ -1,10 +1,30 @@
 package com.amigoscode;
 
+import com.amigoscode._2_developers._1_loops.LoopExercises;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Amigoscode Java Exercises");
         System.out.println("Navigate to each package and run the exercises individually.");
+        //LoopExercises.printMultiplicationTable(3);
+//        int result = LoopExercises.sumWithSkipAndStop(200);
+//        System.out.println("Sum is " + result);
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//       String result = LoopExercises.findInMatrix(matrix,3);
+//        System.out.println(result);
+//         long result = LoopExercises.factorial(5);
+//        System.out.println(result);
+//        LoopExercises.printPyramid(4);
+//        int[] arr = {1,2,3};
+//       String result =  LoopExercises.iterateBackwards(arr);
+//        System.out.println(result);
+        int result = LoopExercises.countUntilMatch(70);
+        System.out.println(result);
     }
 
 }
